f119b6bfcad95ae639528a2f3417279eb551b817
Tazio <tazio2003@gmail.com>
2020-04-29 18:53:45 +0200

    working version

--- esx_invest/server/version.lua
+++ esx_invest/server/version.lua
@@ -10,10 +10,10 @@ Citizen.CreateThread( function()
     
         if tonumber(curVersion) < tonumber(responseText) then
             print("\n------------------------------------")
-            print("\n"..resourceName.." is outdated, should be:\n"..responseText.." but it is:\n"..curVersion.."\nplease update it from \nhttps://github.com/"..updatePath.."/releases/latest")
+            print("\n"..resourceName.." is outdated,\n"..curVersion.." -> "..responseText.."\nhttps://github.com/"..updatePath.."/releases/latest")
             print("\n------------------------------------")
         end
     end
     
-    PerformHttpRequest("https://raw.githubusercontent.com/"..updatePath.."/master/version", checkVersion, "GET")
+    PerformHttpRequest("https://raw.githubusercontent.com/"..updatePath.."/master/".. GetCurrentResourceName() .."/version", checkVersion, "GET")
 end)
