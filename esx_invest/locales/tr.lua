c6955d11ab29a6c537a12778f9b4600439dcfe2d
Abdulkadir AKTAŞ <abdulkadir.aktas@hotmail.com>
2020-04-30 15:36:13 +0300

    added tr version

---
+++ esx_invest/locales/tr.lua
@@ -0,0 +1,9 @@
+Locales['tr'] = {
+    ['invalid_amount'] = '~r~Geçersiz değer',
+    ['broke_amount'] = "~r~Yeterli miktarda paran yok",
+    ['sold'] = "~g~Hisse senetiniz satıldı",
+    ['buy'] = "~g~Hisse senetini satın aldınız.",
+    ['added'] = "Senetinize daha fazla yatırım yaptınız",
+    ['unexpected_error'] = "~r~Beklenmeyen bir hata oluştu",
+    ['open_menu'] = "~INPUT_PICKUP~ Menüyü Aç"
+}
